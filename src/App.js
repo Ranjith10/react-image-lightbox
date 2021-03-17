a5849854ff882d34769f646f5c60f20ce59b5b1c
Ranjith kumar <ranjit4072@gmail.com>
2021-03-17 10:48:27 +0530

    Basic example added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './components/BasicExample';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicExample />
+    </div>
+  );
+}
+
+export default App;
